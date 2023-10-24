3a057266b945f243166809410f4b1825b54304f5
B1gLev <kozma.lev2004@gmail.com>
2023-10-24 11:43:10 +0200

    Add project files.

---
+++ SOLSYS/Bolygo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLSYS
+{
+    internal class Bolygo
+    {
+        public string Neve { get; set; }
+        public int Holdak { get; set; }
+        public double Terfogat { get; set; }
+
+        public Bolygo(string sor)
+        {
+            var adat = sor.Split(';');
+            Neve = adat[0];
+            Holdak = int.Parse(adat[1]);
+            Terfogat = double.Parse(adat[2].Replace('.', ','));
+        }
+    }
+}
---
+++ SOLSYS/Program.cs
@@ -0,0 +1,29 @@
+﻿using SOLSYS;
+
+List<Bolygo> bolygok = new();
+using (StreamReader r = new(path: "solsys.txt"))
+{
+    while (!r.EndOfStream)
+        bolygok.Add(new Bolygo(r.ReadLine()));
+}
+
+Console.WriteLine($"{bolygok.Count} bolyóg van a naprendszerben");
+
+var atlagHoldak = bolygok.Average(b => b.Holdak);
+Console.WriteLine($"A nrpednszerben egy bolygónak átlaghosan {atlagHoldak} holdja van.");
+
+var legTerfogatBoly = bolygok.SingleOrDefault(b => b.Terfogat == bolygok.Max(b2 => b2.Terfogat));
+Console.WriteLine($"A legnagyobb téfegoatú bolygó a {legTerfogatBoly.Neve}");
+
+Console.Write("Írd be a keresett bolygó nevét: ");
+var bolyNev = Console.ReadLine();
+Console.WriteLine(bolygok.SingleOrDefault(b => b.Neve == bolyNev) != null ? 
+    "Van ilyen bolygó a naprendszerben." : 
+    "Sajnos nincs ilyen nevű bolygó a naprendszerben.");
+
+Console.Write("Írj be egy egész számot: ");
+var szam = int.Parse(Console.ReadLine());
+var tobbHold = bolygok.Where(b => b.Holdak > szam).Select(b => b.Neve).ToArray();
+Console.WriteLine("A következő bolygóknak van 10-nál/nél több holdja:");
+foreach (var i in tobbHold)
+    Console.Write($"{i}, ");
